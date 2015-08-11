a1283b2d2f0d4e89afbd787b64a0b303897dcf7e
ghoulsblade <ghoulsblade@schattenkind.net>
2015-08-11 13:31:18 +0200

    test02-new

--- test02.java
+++ test02.java
@@ -4,7 +4,7 @@ public class TT2Decoder {
     
     int getTraceYear ()
     {
-    	return trace_year; // TODO! from file header
+    	return trace_year; // year not available from trace header (only month+day+time), so assume current year. 
     }
     
 }
